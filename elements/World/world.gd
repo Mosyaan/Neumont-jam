5f548a4e4cdc9aa5a7eb23285b6c9d940972a1a8
Ilya Chertovskikh <ilyachertovskikh@yandex.ru>
2025-08-09 14:47:57 +0300

    shit

---
+++ elements/World/world.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+
+func _ready() -> void:
+	polygon_2d.polygon = collision_polygon_2d.polygon
+
+
+
+"""
+# Called when the node enters the scene tree for the first time.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+"""
