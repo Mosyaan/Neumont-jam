3e48701fea367e6b75df9b2e0589b357ab711476
Matvey Kuznetsov <worldmex1@gmail.com>
2025-08-10 02:20:25 +0700

    feat: add player health
    - add hit\die sound
    - add health bar above player
    - add damage mechanic

--- elements/Player/player.gd
+++ elements/Player/player.gd
@@ -4,29 +4,54 @@ const SPEED = 150
 const ACCELERATION = 800
 const FRICTION = 1250
 
+var health = 100
+var max_health = 100
+
+@onready var healthbar = %ProgressBar;
+
+func _process(delta: float) -> void:
+	healthbar.value = health
+	healthbar.max_value = max_health
+
 func _physics_process(delta: float):
 	var vec = Vector2.ZERO
 	vec.x = Input.get_axis("move_left", "move_right")
 	vec.y = Input.get_axis("move_up", "move_down")
+	vec = vec.normalized()
+
+	%sprite.flip_h = vec.x < 0
 	
-	
-	
-	if vec.x > 0: %sprite.flip_h = false
-	elif vec.x < 0: %sprite.flip_h = true
-	
-	handle_movement(vec.normalized(), delta)
+	handle_movement(vec, delta)
 	handle_animations(vec)
 	
 	move_and_slide()
 
 
-func handle_movement(vec, delta):
+func handle_movement(vec: Vector2, delta: float):
 	velocity.x = move_toward(velocity.x, SPEED * vec.x, ACCELERATION * delta)
 	velocity.y = move_toward(velocity.y, SPEED * vec.y, ACCELERATION * delta)
 
 
-func handle_animations(vec):
-	if vec.x or vec.y:
+func handle_animations(vec: Vector2):
+	if vec.length() > 0:
 		if %sprite.animation != "Walking": %sprite.animation = "Walking"
 	else:
 		if %sprite.animation != "Idle": %sprite.animation = "Idle"
+
+func hit(damage: float) -> void:
+	health -= damage
+	
+	if health <= 0:
+		die()
+	else:
+		$HitSound.play()
+	
+func acelerate(vector: Vector2) -> void:
+	print_debug(vector)
+	handle_movement(vector, 0.1)
+	handle_animations(vector)
+	move_and_slide()
+
+func die() -> void:
+	$DieSound.play()
+	get_parent().remove_child(get_node('.'))
--- elements/World/world.gd
+++ elements/World/world.gd
@@ -1,16 +1,38 @@
 extends Node2D
 
+var cooldown_max = 0.5
+var time_past = 0
+var knonkback_force = 200
+
 func _ready() -> void:
 	pass
+	
+var bodies = {}
 
 func _process(delta: float) -> void:
-	pass
+	for bodyId in bodies:
+		var body = bodies[bodyId]
+		if body.cooldown > 0: 
+			body.cooldown -= delta
+		
+		if body.cooldown <= 0:
+			deal_damage(5, bodyId)
 
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if "hit" in body:
+		var bodyId = body.get_instance_id()
+		bodies[bodyId] = {
+			cooldown = 0,
+			node = body
+		}
 
-"""
-# Called when the node enters the scene tree for the first time.
+func deal_damage(damage: float, bodyId: int) -> void:
+	var body = bodies[bodyId];
+	body.node.hit(damage)
+	body.cooldown = cooldown_max
+	if "acelerate" in body.node:
+		var force = $Area2D.global_position.direction_to(body.node.global_position)
+		body.node.acelerate(force * knonkback_force)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
-"""
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	bodies.erase(body.get_instance_id())
